c233e9db629dee2e09bf2cb6a010386e9bf5a96a
Taylor Miley <coachtaylormiley@gmail.com>
2016-02-03 12:26:36 -0600

    first commit, api not working yet

---
+++ WeatherModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloUWP
+{
+    public class WeatherModel
+    {
+        public string Name { get; set; }
+        public int Temperature { get; set; }
+
+
+        public WeatherModel(string name, int temp)
+        {
+            Name = name;
+            Temperature = temp;
+        }
+
+        public WeatherModel() { }
+    }
+}
